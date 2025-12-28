ac7981ae3096848a11a51676bcd5945b1e4172e0
רועי רונן <maroy.b.i@gmail.com>
2025-12-28 12:27:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6556442138-3a4b2",
+  "appId": "1:177575203360:web:06f277273016ac1bbfa245",
+  "apiKey": "AIzaSyDCuhOc3oRzT1ftUWmAFlJVLAndk7QYbv4",
+  "authDomain": "studio-6556442138-3a4b2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "177575203360"
+};
